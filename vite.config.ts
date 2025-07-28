04750789638cb6052f5744cebc9ba300f9375715
Shea Martin <shea@brewninja.net>
2025-07-28 12:16:41 -0300

    remove react dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import "vitest/config";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -8,9 +7,6 @@ const testFiles = ["./src/**/*.test.ts", "./src/**/*.test.tsx"];
 const config = defineConfig({
   test: {
     globals: true,
-    environment: "jsdom",
-    // setupFiles: ['./config/jest-setup.ts'],
-    // globalSetup: ['./config/testGlobalSetup.ts'],
     include: testFiles,
 
     // Enable auto-mocking for __mocks__ folders
@@ -18,12 +14,11 @@ const config = defineConfig({
     // clearMocks: true,
     // restoreMocks: true,
   },
-  // build: {
-  //   emptyOutDir: true,
-  //   sourcemap: true,
-  // },
-  // plugins: [tsconfigPaths(), react()],
-  plugins: [tsconfigPaths(), react()],
+  build: {
+    emptyOutDir: true,
+    sourcemap: true,
+  },
+  plugins: [tsconfigPaths()],
 });
 
 // console.log(`VITE CONFIG:\n${JSON.stringify(config.resolve.alias, null, 2)}`);
