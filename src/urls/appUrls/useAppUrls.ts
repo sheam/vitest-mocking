7ce93f79e26892aca83043c0d054fa10eff1205c
Shea Martin <shea@brewninja.net>
2025-07-28 12:11:18 -0300

    simplified tsconfig, matches working example, but still doesn't work

--- src/urls/appUrls/useAppUrls.ts
+++ src/urls/appUrls/useAppUrls.ts
@@ -3,7 +3,7 @@ import { IAppUrlsContext } from '@urls/types';
 // export const AppUrlsContext = createContext<IAppUrlsContext>({} as IAppUrlsContext);
 
 export function useAppUrls(): IAppUrlsContext {
-  // throw new Error('NOT CALLING THE MOCK!!!!');
+  // throw new Error ('NOT CALLING THE MOCK!!!!');
   // return useContext(AppUrlsContext);
   return {
     appUrls: {
